c5b4f4c288c56410725e3cd66204f6dcb4990503
César Henrique de Figueiredo <cesar.sprite@gmail.com>
2020-08-15 18:04:05 -0300

    responsivo 15/08

--- js/scripts.js
+++ js/scripts.js
@@ -42,4 +42,38 @@ $(document).ready(function() {
         }
     }
 
+    // scroll para seções
+    let navBtn = $('.nav-item');
+    
+    let bannerSection = $('#mainSlider');
+    let cardapioSection = $('#cardapio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+
+        let btnId = $(this).attr('id');
+
+        console.log(btnId);
+
+        if(btnId == 'home-menu') {
+            scrollTo = bannerSection;
+        } else if(btnId == 'cardapio-menu') {
+            scrollTo = cardapioSection;
+        } else if(btnId == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 10
+        }, 1500);
+    
+
+    });
+
+    
+
 });
