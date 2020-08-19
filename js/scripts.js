03fb2c38af3cc48b5abe9b4367ad28aca2f7d640
César Henrique de Figueiredo <cesar.sprite@gmail.com>
2020-08-18 21:56:46 -0300

    atualização  18/08

--- js/scripts.js
+++ js/scripts.js
@@ -77,3 +77,19 @@ $(document).ready(function() {
     
 
 });
+// botão topo
+    
+window.onscroll = function() {
+    scroll();
+};
+function scroll() {
+    let btn = document.getElementById("btnTop");
+    if(document.documentElement.scrollTop > 50) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+function backToTop() {
+    document.documentElement.scrollTop = 0;
+}
